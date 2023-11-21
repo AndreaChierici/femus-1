454ca0e642d7a6d3284b0fd6ef891f96a7573335
Andrea Chierici <anchieri@ttu.edu>
2023-11-21 11:38:45 -0600

    Starting implementation of the class NOM

---
+++ applications/NOM/ex1/ex1.cpp
@@ -0,0 +1,31 @@
+#include "FemusInit.hpp"
+#include "MultiLevelSolution.hpp"
+#include "MultiLevelProblem.hpp"
+#include "VTKWriter.hpp"
+#include "TransientSystem.hpp"
+#include "LinearImplicitSystem.hpp"
+
+#include "NumericVector.hpp"
+#include "adept.h"
+
+#include "CurrentElem.hpp"
+#include "ElemType_template.hpp"
+
+#include "petsc.h"
+#include "petscmat.h"
+#include "PetscMatrix.hpp"
+
+#include "slepceps.h"
+
+#include "PetscMatrix.hpp"
+
+#include "Assemble_jacobian.hpp"
+
+
+using namespace femus;
+
+
+int main(int argc, char** argv)
+{
+    
+}
